4f9a623bb667b74bc2a6726835eb0759f32ef889
Afzal Sayed <afzalsayed96@gmail.com>
2020-08-29 14:49:20 +0530

    master

---
+++ build/precache-manifest.fef2f10281342d7f931f824692decf74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df4efe71e6f4f93f8b2bf6054632438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a060f828bdd7a11a963",
+    "url": "/static/css/main.b898a539.chunk.css"
+  },
+  {
+    "revision": "d7fde601ad34e13b6a54",
+    "url": "/static/js/2.82bb2054.chunk.js"
+  },
+  {
+    "revision": "8a060f828bdd7a11a963",
+    "url": "/static/js/main.5adcc40c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
